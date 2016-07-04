66e61f0265c45c24a9dbed8658207e5e0e428ef9
Ivan Giuliani <giuliani.v@gmail.com>
2016-07-04 23:31:02 +0100

    Add missing tests for keySet() and values()

--- src/test/java/com/github/kratorius/cuckoohash/ViewsTest.java
+++ src/test/java/com/github/kratorius/cuckoohash/ViewsTest.java
@@ -3,6 +3,7 @@ package com.github.kratorius.cuckoohash;
 import junit.framework.TestCase;
 
 import java.util.AbstractMap;
+import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
@@ -43,5 +44,61 @@ public class ViewsTest extends TestCase {
     }
   }
 
-  // TODO keySet() and values()
+  public void testKeySet() {
+    final int size = 1024;
+    CuckooHashMap<String, Integer> m = new CuckooHashMap<>();
+
+    for (int i = 0; i < size; i++){
+      m.put("key-" + i, i);
+    }
+
+    // keySet() returns a set and therefore there's no guarantee on the ordering of the keys.
+    Set<String> keys = m.keySet();
+    assertEquals(size, keys.size());
+
+    for (int i = 0; i < size; i++){
+      assertTrue(keys.contains("key-" + i));
+    }
+
+    // Override a key.
+    m.put("key-123", 2134);
+    keys = m.keySet();
+    assertEquals(size, keys.size());
+
+    for (int i = 0; i < size; i++){
+      assertTrue(keys.contains("key-" + i));
+    }
+  }
+
+  public void testValues_unique() {
+    final int size = 1024;
+    CuckooHashMap<String, Integer> m = new CuckooHashMap<>();
+
+    for (int i = 0; i < size; i++){
+      m.put("key-" + i, i);
+    }
+
+    Collection<Integer> values = m.values();
+    assertEquals(size, values.size());
+
+    for (int i = 0; i < size; i++){
+      assertTrue(values.contains(i));
+    }
+  }
+
+  public void testValues_duplicates() {
+    final int size = 1024;
+    CuckooHashMap<String, Integer> m = new CuckooHashMap<>();
+
+    for (int i = 0; i < size; i++){
+      m.put("key-" + i, i / 2);
+    }
+
+    Collection<Integer> values = m.values();
+    assertEquals(size, values.size());
+
+    for (int i = 0; i < size; i++){
+      assertTrue(values.contains(i / 2));
+    }
+  }
 }
